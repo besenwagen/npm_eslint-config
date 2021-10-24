41b9bdb2ad9e65df3d905fca6d9db86ee9fb401d
Eric Bednarz <dotgitignore@outlook.com>
2021-10-24 19:50:15 +0200

    feat(rules): use less rules
    BREAKING CHANGE: don't extend recommended

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # `@eric.bednarz.dev/eslint-config` [![npm version][npm-image]][npm-url]
 
-> Environment-agnostic ECMAScript 2018 base configuration.
-  Extend with module and host object configuration as needed.
+> Environment-agnostic ECMAScript 2018 base configuration. Extend with module
+> and host object configuration as needed.
 
 ## Peer dependencies
 
-- `eslint@6.2`
+- `eslint@8.1`
 
 ## Installation
 
@@ -22,4 +22,4 @@
 MIT
 
 [npm-image]: https://img.shields.io/npm/v/@eric.bednarz.dev/eslint-config.svg?style=flat-square
-[npm-url]:   https://www.npmjs.com/package/@eric.bednarz.dev/eslint-config
+[npm-url]: https://www.npmjs.com/package/@eric.bednarz.dev/eslint-config
--- source/eslintrc.js
+++ source/eslintrc.js
@@ -1,31 +1,23 @@
 /* global module */
 const ERROR = 'error';
-const ALWAYS = 'always';
-const NEVER = 'never';
-const AS_NEEDED = 'as-needed';
 const OFF = 'off';
-const MULTILINE_ALWAYS = 'always-multiline';
 const READONLY = 'readonly';
-const INDENT_SIZE = 2;
+const INDENT = 2;
+const MAX_CALLBACKS = 1;
 const MAX_COMPLEXITY = 3;
 const MAX_DEPTH = 1;
-const MAX_EMPTY_LINES = 1;
-const MAX_LINE_LENGTH = 72;
 const MAX_PARAMETERS = 3;
 const MAX_STATEMENTS = 7;
-const MIN_ID_LENGTH = 2;
 
 module.exports = {
-  extends: 'eslint:recommended',
   parserOptions: {
-    ecmaVersion: 2020,
+    ecmaVersion: 13,
     sourceType: 'module',
   },
   env: {
-    es6: true,
+    es2021: true,
   },
   globals: {
-    BigInt: READONLY,
     URL: READONLY,
     console: READONLY,
     setInterval: READONLY,
@@ -34,165 +26,35 @@ module.exports = {
     clearTimeout: READONLY,
   },
   rules: {
-    // complexity
+    // code style
+    'comma-dangle': [ERROR, 'always-multiline'],
+    indent: [ERROR, INDENT],
+    'no-extra-semi': [ERROR],
+    semi: [ERROR],
+    // simplicity
     complexity: [ERROR, MAX_COMPLEXITY],
     'max-depth': [ERROR, MAX_DEPTH],
-    'no-nested-ternary': [ERROR],
-
-    // clarity
-    'max-classes-per-file': [ERROR],
-    'max-statements-per-line': [ERROR],
-    'no-else-return': [ERROR],
-    'no-empty-function': [ERROR],
-    'no-implicit-coercion': [ERROR],
-    'no-lonely-if': [ERROR],
-    'no-magic-numbers': [ERROR],
-    'no-negated-condition': [ERROR],
-    'no-param-reassign': [ERROR],
-    'no-shadow': [ERROR],
-    'no-unneeded-ternary': [ERROR],
-    'symbol-description': [ERROR],
-
-    // functions
+    'max-nested-callbacks': [ERROR, MAX_CALLBACKS],
     'max-params': [ERROR, MAX_PARAMETERS],
     'max-statements': [ERROR, MAX_STATEMENTS],
-    'func-style': [ERROR, 'declaration', {
-      allowArrowFunctions: true,
-    }],
-    'func-names': [ERROR, ALWAYS],
-    'arrow-body-style': [ERROR, NEVER],
-    'arrow-parens': [ERROR, AS_NEEDED],
-    'no-confusing-arrow': [ERROR],
-
-    // verbosity
-    'comma-dangle': [ERROR, {
-      arrays: MULTILINE_ALWAYS,
-      objects: MULTILINE_ALWAYS,
-      imports: MULTILINE_ALWAYS,
-      exports: MULTILINE_ALWAYS,
-      functions: NEVER,
-    }],
-    curly: [ERROR],
-    'default-case': [ERROR],
-    eqeqeq: [ERROR],
-    'one-var': [ERROR, NEVER],
-    semi: [ERROR],
-
-    // bug guards
-    radix: [ERROR],
-    'guard-for-in': [ERROR],
-
-    // quotes
-    'quote-props': [ERROR, AS_NEEDED],
-    quotes: [ERROR, 'single', {
-      avoidEscape: true,
-    }],
-
-    // naming
-    camelcase: [ERROR],
-    'id-length': [ERROR, {
-      min: MIN_ID_LENGTH,
-      exceptions: ['x', 'y'],
-    }],
-    'new-cap': [ERROR],
-    'no-underscore-dangle': [ERROR],
-
-    // opt out
-    'no-array-constructor': [ERROR],
-    'no-async-promise-executor': [ERROR],
-    'no-await-in-loop': [ERROR],
-    'no-caller': [ERROR],
-    'no-console': [ERROR, {
-      allow: [
-        'assert',
-        'error',
-        'info',
-        'warn',
-      ],
-    }],
-    'no-duplicate-imports': [ERROR, {
-      includeExports: true,
-    }],
-    'no-eq-null': [ERROR],
-    'no-eval': [ERROR],
-    'no-extend-native': [ERROR],
-    'no-implied-eval': [ERROR],
-    'no-inline-comments': [ERROR],
-    'no-multi-assign': [ERROR],
-    'no-new-object': [ERROR],
-    'no-plusplus': [ERROR],
-    'no-proto': [ERROR],
-    'no-restricted-syntax': [ERROR, {
-      selector: 'ExportDefaultDeclaration',
-      message: 'Prefer named exports.',
-    }],
-    'no-throw-literal': [ERROR],
-    'no-var': [ERROR],
-    'no-void': [ERROR],
-    'no-with': [ERROR],
+    // nannying
+    'no-delete-var': [ERROR],
+    'no-dupe-args': [ERROR],
+    'no-global-assign': [ERROR],
+    'no-implicit-globals': [ERROR],
+    'no-param-reassign': [ERROR],
+    'no-redeclare': [ERROR],
+    'no-shadow': [ERROR],
+    'no-undef': [ERROR],
+    'no-unused-vars': [ERROR],
+    'no-useless-escape': [ERROR],
+    'valid-typeof': [ERROR],
+    // ES 2015+
+    'constructor-super': [ERROR],
+    'no-dupe-class-members': [ERROR],
+    'no-unused-private-class-members': [ERROR],
     'prefer-rest-params': [ERROR],
     'prefer-spread': [ERROR],
-    'prefer-template': [ERROR],
-
-    // formatting
-    'array-bracket-spacing': [ERROR],
-    'brace-style': [ERROR],
-    'comma-spacing': [ERROR],
-    'comma-style': [ERROR],
-    'computed-property-spacing': [ERROR],
-    'dot-location': [ERROR, 'property'],
-    'func-call-spacing': [ERROR],
-    indent: [ERROR, INDENT_SIZE],
-    'key-spacing': [ERROR],
-    'keyword-spacing': [ERROR],
-    'line-comment-position': [ERROR, {
-      position: 'above',
-    }],
-    'lines-around-comment': [ERROR, {
-      allowArrayStart: true,
-      allowBlockStart: true,
-      allowClassStart: true,
-      allowObjectStart: true,
-      beforeLineComment: true,
-      beforeBlockComment: true,
-    }],
-    'lines-between-class-members': [ERROR],
-    'max-len': [ERROR, MAX_LINE_LENGTH, {
-      ignoreUrls: true,
-    }],
-    'multiline-comment-style': [ERROR, 'starred-block'],
-    'no-multi-spaces': [ERROR],
-    'no-multiple-empty-lines': [ERROR, {
-      max: MAX_EMPTY_LINES,
-    }],
-    'no-trailing-spaces': [ERROR],
-    'object-property-newline': [ERROR],
-    'padded-blocks': [ERROR, NEVER],
-    'padding-line-between-statements': [ERROR, {
-      blankLine: 'always',
-      prev: '*',
-      next: 'return',
-    }],
-    'semi-spacing': [ERROR],
-    'semi-style': [ERROR],
-    'space-before-function-paren': [ERROR, NEVER],
-    'space-in-parens': [ERROR],
-    'spaced-comment': [ERROR, ALWAYS, {
-      line: {
-        markers: [
-          '#region',
-          '#endregion',
-          '/',
-        ],
-      },
-      block: {
-        markers: [
-          '!',
-        ],
-      },
-    }],
-    'switch-colon-spacing': [ERROR],
-    'template-tag-spacing': [ERROR],
   },
   overrides: [
     {
@@ -200,26 +62,8 @@ module.exports = {
         '*.test.js',
       ],
       rules: {
-        'arrow-body-style': [OFF],
-        camelcase: [OFF],
-        'comma-dangle': [ERROR, 'only-multiline'],
-        curly: [OFF],
-        'func-call-spacing': [OFF],
-        'id-length': [OFF],
-        indent: [OFF],
-        'max-params': [OFF],
         'max-statements': [OFF],
-        'multiline-comment-style': [OFF],
-        'no-array-constructor': [OFF],
-        'no-empty-function': [OFF],
-        'no-magic-numbers': [OFF],
-        'no-plusplus': [OFF],
-        'no-restricted-syntax': [OFF],
-        'no-unexpected-multiline': [OFF],
-        'no-void': [OFF],
-        'padding-line-between-statements': [OFF],
-        semi: [ERROR, NEVER],
-        'semi-style': [OFF],
+        semi: [OFF],
       },
     },
   ],
